abcaa2158c0f6e0a21b62beb79c28b2e9e61b8c3
maxud <mlenon13@gmail.com>
2017-10-27 19:03:45 +0300

    CocoaPods

---
+++ Lesson_21/Lesson_21/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_21
+//
+//  Created by maxud on 24.10.2017.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson_21/Lesson_21/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson_21
+//
+//  Created by maxud on 24.10.2017.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
